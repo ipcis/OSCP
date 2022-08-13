76e55f929546bb035e3ae1a1632b19d4653c826f
Core|Threat <30553333+ipcis@users.noreply.github.com>
2022-08-13 08:49:53 +0200

    Update enum.sh

--- enum.sh
+++ enum.sh
@@ -1,1 +1,19 @@
+#Info about me
+id || (whoami && groups) 2>/dev/null
+#List all users
+cat /etc/passwd | cut -d: -f1
+#List users with console
+cat /etc/passwd | grep "sh$"
+#List superusers
+awk -F: '($3 == "0") {print}' /etc/passwd
+#Currently logged users
+w
+#Login history
+last | tail
+#Last log of each user
+lastlog
 
+#List all users and their groups
+for i in $(cut -d":" -f1 /etc/passwd 2>/dev/null);do id $i;done 2>/dev/null | sort
+#Current user PGP keys
+gpg --list-keys 2>/dev/null
